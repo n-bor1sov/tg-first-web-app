42bc5857907b31d972209ebeb62b5a636a915de3
n-bor1so <n.bor1sov@yandex.ru>
2023-06-21 11:27:36 +0300

    header4

--- src/componemts/Header/Header.jsx
+++ src/componemts/Header/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Button from '../Button/Button';
-import { useTelegram } from '../hooks/useTelegram';
+import { useTelegram } from '../../hooks/useTelegram';
 
 const Header = () => {
-    const {tg, user, onClose} = useTelegram();
+    const {user, onClose} = useTelegram();
 
     return (
         <div className={'header'}>
