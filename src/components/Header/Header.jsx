dd88f491f01cfecef70288ecb89eb666c4875488
n-bor1so <n.bor1sov@yandex.ru>
2023-06-22 23:42:07 +0300

    progress bar3

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,11 +9,11 @@ const Header = () => {
     return (
         <div className={"wrapper"}>
             <div className={'header'}>
-                <div className='name'>
+                <div className={'name'}>
                     {user?.first_name + " " + user?.last_name}
                 </div>
             </div>
-            
+
         </div>
         
     );
