67bd250e6fd23874b32cb76ea16c4bde426e0f06
n-bor1so <n.bor1sov@yandex.ru>
2023-06-22 20:54:38 +0300

    Challenge list10

--- src/components/Challenge/Challenge.jsx
+++ src/components/Challenge/Challenge.jsx
@@ -10,15 +10,15 @@ const Challenge = (item) => {
     
     return (
         <div className={"challenge"}>
-            <div className={"module"}>
+            <div className={"challenge-side-module"}>
                 <div className={"img"}/>
                 <div className={"points"}>{item.challenge.points}</div>
             </div>
-            <div className={"module"}>
+            <div className={"challenge-mid-module"}>
                 <div className={"title"}>{item.challenge.title}</div>
                 <div className={"description"}>{item.challenge.description}</div>
             </div>
-            <div className={"module"}>
+            <div className={"challenge-side-module"}>
                 <Button className={"finish-chellange"} onClick={finishHandler}>
                     Done
                 </Button>
