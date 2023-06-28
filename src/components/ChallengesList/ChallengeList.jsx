b1f4fb801825251d0676972f1a8bd96cc98ded9e
n-bor1so <n.bor1sov@yandex.ru>
2023-06-28 19:05:22 +0300

    list

--- src/components/ChallengesList/ChallengeList.jsx
+++ src/components/ChallengesList/ChallengeList.jsx
@@ -5,7 +5,7 @@ import { useTelegram } from "../../hooks/useTelegram";
 import Button from "../Button/Button";
 
 
-const dataBase = {
+var dataBase = {
     users: [ {id: 894797521, 
         challenges: [{id: 0, progress: 5, need: 10, isFinished: false}, {id: 1, progress: 10, need: 10, isFinished: true}, {id: 2, progress: 10, need: 5000, isFinished: false}, {id: 3, progress: 0, need: 1, isFinished: false}]
     }
@@ -25,9 +25,12 @@ const ChallengeList = () => {
     let finished = [];
 
     const {user} = useTelegram();
-    
 
-    const client = dataBase.users.find({id: 894797521})
+    console.log(dataBase.users)
+    
+    const client = dataBase.users.find({id: 894797521});
+
+    console.log(client)
 
     client.challenges.forEach(e => {
         if(e.isFinished) {
