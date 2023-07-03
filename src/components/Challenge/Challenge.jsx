f3d2be7d2a605069d884c02847d321e696402b9e
n-bor1so <n.bor1sov@yandex.ru>
2023-07-03 09:30:01 +0300

    icons8

--- src/components/Challenge/Challenge.jsx
+++ src/components/Challenge/Challenge.jsx
@@ -4,10 +4,10 @@ import Button from "../Button/Button"
 import ProgressBar from "../ProgressBar/ProgressBar";
 
 const images = [
-    "https://1.downloader.disk.yandex.ru/preview/fdbb9d99fe67471361f1941fa08c805b005a949c0c514316e271a3d5e3ef4007/inf/VKAaGjA6mvI5Iee-Do844qKFN9_nflGC39FCapIuMsDp3aOGEp9FLPzZl7YS2fupimi9F4r3zPafvnvC3RE4CQ%3D%3D?uid=590435900&filename=0.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=590435900&tknv=v2&size=1898x947",
-    "https://1.downloader.disk.yandex.ru/preview/fdbb9d99fe67471361f1941fa08c805b005a949c0c514316e271a3d5e3ef4007/inf/VKAaGjA6mvI5Iee-Do844qKFN9_nflGC39FCapIuMsDp3aOGEp9FLPzZl7YS2fupimi9F4r3zPafvnvC3RE4CQ%3D%3D?uid=590435900&filename=0.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=590435900&tknv=v2&size=1898x947",
-    "https://2.downloader.disk.yandex.ru/preview/d41a5335c7ebe94485c74bbfbb0c9248aa2726e701e326e98a6eb82f174cc38f/inf/rmhKwYjmw1kEt__riQUMf-H9CYlqr-QlLtTQ848KYMU2eD7Ke1Bcdj2pUKTlD7UwG2bRTF7g0GsAM0vVYOgoqw%3D%3D?uid=590435900&filename=2.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=590435900&tknv=v2&size=1898x947",
-    "https://2.downloader.disk.yandex.ru/preview/9d38f4db83ebf77f322e885ec3d5fd27df29369da041915b0f79d3b4923a628e/inf/iF4brzzxLtGMfsf2tErT1UrReJQBBGwJiUSIy3MQV_TxbowqZQ-NHg6-P8L186oELY80HI3w06qfCvZ3uCCoxA%3D%3D?uid=590435900&filename=3.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=590435900&tknv=v2&size=1898x947"
+    "https://github.com/n-bor1sov/tg-first-web-app/blob/main/src/components/static/0.png",
+    "https://github.com/n-bor1sov/tg-first-web-app/blob/main/src/components/static/1.png",
+    "https://github.com/n-bor1sov/tg-first-web-app/blob/main/src/components/static/2.png",
+    "https://github.com/n-bor1sov/tg-first-web-app/blob/main/src/components/static/3.png"
 ]
 
 const Challenge = (item) => {
